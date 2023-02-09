f59fafe7204aa5d9cbdae84fb854908ff4d634a8
Abdallah Imam Mohammed <Abd_2015_Abd@outlook.com>
2023-02-09 11:30:41 +0200

    Add Layout view

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ namespace BookStore.Controllers
 {
 	public class HomeController : Controller
 	{
-		public string Index()
+		public ViewResult Index()
 		{
-			return "Hello Abdo";
+			return View();
 		}
 	}
 }
