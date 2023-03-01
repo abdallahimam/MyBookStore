ff7c4ce6967417c142f8525ede13b2b03b5f9ef9
Abdallah Imam Mohammed <Abd_2015_Abd@outlook.com>
2023-03-01 19:29:34 +0200

    update

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 builder.Services.Configure<IdentityOptions>(options =>
 {
     options.Password.RequireUppercase = true;
+    options.Password.RequireUppercase = false;
     options.SignIn.RequireConfirmedEmail = true;
     options.Lockout.MaxFailedAccessAttempts = 3;
     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
